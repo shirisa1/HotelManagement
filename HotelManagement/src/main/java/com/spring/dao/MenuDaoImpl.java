1f345a5e10d671efb69c30b60455b628a59ea57f
Shahan Mir <48497722+RoninSenpaii@users.noreply.github.com>
2019-09-26 02:18:05 -0500

    v3

---
+++ HotelManagement/src/main/java/com/spring/dao/MenuDaoImpl.java
@@ -0,0 +1,29 @@
+package com.spring.dao;
+
+import java.util.List;
+
+import com.spring.model.MenuItem;
+
+public class MenuDaoImpl implements MenuDao{
+
+	public void addMenuItem(MenuItem m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateMenuItem(MenuItem m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<MenuItem> listMenuItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void removeMenuItem(int menuID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
